07fcdf62ae84e1e519cb06ba25a873a3e27ea496
yhryb <155826018+yhryb@users.noreply.github.com>
2024-10-05 18:24:23 +0300

    Update simpleService.sh

--- simpleService.sh
+++ simpleService.sh
@@ -3,6 +3,9 @@
 while true
 do
     echo "$(date):"
-    top -b -o +%MEM | head -n 17
+    top -b -o +%MEM | head -n 17 
+    #top for memory usage, -b for batch mode, -o +%MEM for memory usage output in ascending order; 
+    #head -n 17 so the script outputs only the first 17 lines
     sleep 180
+    #repeats every 3 minutes
 done
