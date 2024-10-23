9102e3324e3404883b85b23dfcd0f5334c231391
yhryb <155826018+yhryb@users.noreply.github.com>
2024-10-23 21:45:06 +0300

    Update apiServer.sh

--- apiServer.sh
+++ apiServer.sh
@@ -30,5 +30,50 @@ echo "Ok. Ready."
 
 log "Handshake complete with surname: $someSurname and DNS server: $dnsServer"
 
+fetchFromDB() {
+  local searchField=$1
+  local searchValue=$2
+  result=$(grep -i "$searchValue" db.txt | awk -F';' -v field="$searchField" '{ print $field }')
+  if [[ -z "$result" ]]; then
+    echo "No match found for $searchValue."
+  else
+    echo "$result"
+  fi
+}
+
+while true; do
+  echo "Enter API command:"
+  read apiCommand
+
+  case "$apiCommand" in
+    "get_album_by_song")
+      echo "Enter song name:"
+      read songName
+      album=$(fetchFromDB 3 "$songName")
+      echo "Album: $album"
+      log "Fetched album for song '$songName': $album"
+      ;;
+    
+    "get_songs_by_performer")
+      echo "Enter performer name:"
+      read performerName
+      songs=$(fetchFromDB 1 "$performerName")
+      echo "Songs by $performerName: $songs"
+      log "Fetched songs by performer '$performerName': $songs"
+      ;;
+    
+    "exit")
+      echo "Closing connection."
+      log "Client disconnected."
+      break
+      ;;
+
+    *)
+      echo "Invalid command."
+      log "Invalid command received: $apiCommand"
+      ;;
+  esac
+done
+
 
 
