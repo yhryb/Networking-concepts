5cbff4938f3462adc1915d9c01003ecc29130cee
yhryb <155826018+yhryb@users.noreply.github.com>
2024-11-05 18:53:45 +0200

    Create configureSystem3.sh

---
+++ configureSystem3.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#install apache
+sudo apt update
+sudo apt install -y apache2 socat
+
+#listen on port 1000 not 80
+sudo sed -i 's/Listen 80/Listen 10000/' /etc/apache2/ports.conf
+sudo systemctl restart apache2
+
+#restrict access to port 10000
+sudo iptables -A INPUT -p tcp --dport 10000 -s 127.0.0.1 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 10000 -j DROP
+
+#copy html files
+sudo cp index.html error.html /var/www/html/
+
+#enable and start proxyServer service
+sudo systemctl enable proxyServer
+sudo systemctl start proxyServer
